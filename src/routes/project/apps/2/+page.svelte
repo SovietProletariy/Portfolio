158d508d7668e74f579ff451df5a88e1d28368f4
SovietProletariy <petrenkob444@gmail.com>
2023-06-24 20:22:45 +0300

    dsdsdsdsdsd

---
+++ src/routes/project/apps/2/+page.svelte
@@ -0,0 +1,88 @@
+<script>
+    let expenses = [];
+
+    let newName ='';
+    let newAmount = '';
+
+    function addExpense(){
+        const obj ={
+            name : newName,
+            amount: parseInt(newAmount),
+        };
+        expenses.push(obj);
+        expenses = expenses
+        newName ='';
+        newAmount = '';
+    }
+
+    function deleteExpense(){
+        expenses.slice(index, 1)
+        expenses = expenses
+    }
+</script>
+<main>
+    <h1>Учет расходов</h1>
+    <form on:submit|preventDefault={addExpense}>
+        <input bind:value={newName}/>
+        <input bind:value={newAmount} />
+        <button class="add-todo"on:click={addExpense}><span>Добавить</span></button>
+    </form>
+    <thead>
+        <tr>
+            <th>Название</th>
+            <th>Сумма</th>
+            <th>Деньги</th>
+        </tr>
+    </thead>
+    <tbody>
+        {#each expenses as expenses, i}
+            <tr>
+                <td><div>{expenses.name}</div></td>
+                <td><div>{expenses.amount}</div></td>
+                <td><button on:click={()=> deleteExpense(i)}>Удалить</button></td>
+            </tr>
+        {/each}
+</tbody>
+       
+</main>
+<style>
+    h1{
+        font-size: 24px;
+        margin-bottom: 20px;
+    }
+    button{
+        padding: 5px 10px;
+        background-color: #4caf50;
+        color: white;
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+    }
+    th{
+        background-color: #eee;
+    }
+    th, td{
+        border: 1px solid #ccc;
+        padding: 8px;
+        text-align: left;
+    }
+
+    table{
+        margin-top: 20px;
+        border-collapse: collapse;
+        width:100%;
+    }
+
+    input[type=text]{
+        padding: 5px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        margin-right: 10px;
+        width: 150px;
+    }
+
+    label{
+        display: block;
+        margin-bottom: 10px;
+    }
+</style>
